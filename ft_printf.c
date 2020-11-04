2406443029341dbc9353fec8277cd97c98a6d6a9
SirMorfield <joppe.koers@gmail.com>
2020-11-04 01:59:39 +0100

    cleanup 1

--- ft_printf.c
+++ ft_printf.c
@@ -17,59 +17,68 @@
 #include <stdlib.h>
 #include <stdarg.h>
 
-int	ft_printf(const char *format, ...)
+char	*do_special(char *special, va_list ap)
+{
+	if (special[1] == 'c')
+		return (c_tostr(va_arg(ap, int)));
+	else if (special[1] == 's')
+		return (s_tostr(va_arg(ap, char *)));
+	else if (special[1] == 'p')
+		return (p_tostr(va_arg(ap, void *)));
+	else if (special[1] == 'd')
+		return (d_tostr(va_arg(ap, long)));
+	else if (special[1] == 'i')
+		return (i_tostr(va_arg(ap, long)));
+	else if (special[1] == 'u')
+		return (u_tostr(va_arg(ap, unsigned long)));
+	else if (special[1] == 'x')
+		return (x_tostr(va_arg(ap, unsigned long)));
+	else if (special[1] == 'X')
+		return (x_upper_tostr(va_arg(ap, unsigned long)));
+}
+
+int		print_result(t_list *list)
+{
+	size_t len;
+
+	len = 0;
+	while (list != NULL)
+	{
+		len += ft_strlen((char *)(list->content));
+		ft_putstr((char *)(list->content));
+		list = list->next;
+	}
+	return ((int)len);
+}
+
+int		ft_printf(const char *format, ...)
 {
 	va_list	ap;
 	char	*s;
-	char	*format1;
+	char	*frmt;
 	char	*special;
 	t_list	*list;
 
-	format1 = (char *)format;
-	va_start(ap, 100);
+	frmt = (char *)format;
 	list = NULL;
+	va_start(ap, format);
 	while (true)
 	{
-		special = ft_strchr(format1, '%');
-		if (special)
+		special = ft_strchr(frmt, '%');
+		if (special == NULL)
 		{
-			if (special - format1 > 0)
-			{
-				ft_lstpush_back(&list, ft_strndup(format1, (size_t)(special - format1)));
-				printf("<%s>\n", (char *)list->content);
-			}
-			if (special[1] == 'c')
-				ft_lstpush_back(&list, c_tostr(va_arg(ap, int)));
-			else if (special[1] == 's')
-				ft_lstpush_back(&list, s_tostr(va_arg(ap, char *)));
-			else if (special[1] == 'p')
-				ft_lstpush_back(&list, p_tostr(va_arg(ap, void *)));
-			else if (special[1] == 'd')
-				ft_lstpush_back(&list, d_tostr(va_arg(ap, long)));
-			else if (special[1] == 'i')
-				ft_lstpush_back(&list, i_tostr(va_arg(ap, long)));
-			else if (special[1] == 'u')
-				ft_lstpush_back(&list, u_tostr(va_arg(ap, unsigned long)));
-			else if (special[1] == 'x')
-				ft_lstpush_back(&list, x_tostr(va_arg(ap, unsigned long)));
-			else if (special[1] == 'X')
-				ft_lstpush_back(&list, x_upper_tostr(va_arg(ap, unsigned long)));
-			if (special[1] != '%')
-				format1 = special + 2;
-		}
-		else
-		{
-			ft_lstpush_back(&list, ft_strdup(format1));
+			ft_lstpush_back(&list, ft_strdup(frmt));
 			break ;
 		}
+		if (special - frmt > 0)
+			ft_lstpush_back(&list, ft_strndup(frmt, (size_t)(special - frmt)));
+		if (special[1] != '%')
+		{
+			ft_lstpush_back(&list, do_special(special, ap));
+			frmt = special + 2;
+		}
 	}
+	print_result(list);
 	va_end(ap);
-	while (list != NULL)
-	{
-		ft_putstr_fd((char *)(list->content), 1);
-		list = list->next;
-	}
-	ft_putstr_fd("\n", 1);
-	// ft_lstclear(&list, &free);
-	return (42);
+	return (0);
 }
